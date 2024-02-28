6b3886dc60faf2c5483b9cb241ca08774e736b3c
DeenankSharma <sharmadeenank@gmail.com>
2024-02-29 05:07:14 +0530

    feat : adds music on/off feature

--- scripts/loadbox.js
+++ scripts/loadbox.js
@@ -1,1 +1,19 @@
-let musicbtn = document.querySelector()
+const musicbtn = document.querySelector(".music_off");
+const musicbtnimg=document.querySelector(".music")
+const musicbgfile=document.querySelector(".mainbg");
+const letsgobtn =document.querySelector(".start_button");
+console.dir(musicbgfile)
+musicbtn.addEventListener("click",()=>{
+    if (musicbtnimg.getAttribute("src")==="styles/volume-up.png") {
+        musicbtnimg.setAttribute("src","styles/no-sound.png");
+        musicbgfile.setAttribute("src"," ");
+    }
+    else{
+        musicbtnimg.setAttribute("src","styles/volume-up.png");
+        musicbgfile.setAttribute("src","music/hill-climb-racing_1.mp3");
+    }
+})
+letsgobtn.addEventListener("click",()=>{
+    window.location.href="index2.html";
+})
+
