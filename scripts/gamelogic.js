61299111b8b9e9d113d80cb55dd909222e6f3218
DeenankSharma <sharmadeenank@gmail.com>
2024-03-01 02:30:14 +0530

    feat : adds road movement

--- scripts/gamelogic.js
+++ scripts/gamelogic.js
@@ -23,9 +23,18 @@ obstacle.addEventListener('animationiteration', () => {
         obstacle.style.left = '50px';
         obstacle.style.right = '';
     }
-    let currentStyle = window.getComputedStyle(obstacle);
-    let animationDuration = currentStyle.getPropertyValue('animation-duration');
-    console.log(parseFloat(animationDuration));
-    animationDuration=(parseFloat(animationDuration)/1.05);
-    obstacle.style.setProperty('animation-duration', animationDuration + 's');
+    // let currentStyle = window.getComputedStyle(obstacle);
+    // let animationDuration = currentStyle.getPropertyValue('animation-duration');
+    // console.log(parseFloat(animationDuration));
+    // animationDuration=(parseFloat(animationDuration)/1.05);
+    // obstacle.style.setProperty('animation-duration', animationDuration + 's');
 });
+
+// let carcoord=car.getBoundingClientRect();
+// console.log(carcoord);
+// let obscoord=obstacle.getBoundingClientRect();
+// console.log(obscoord)
+console.log(obstacle.style.top);
+if((obstacle.style.top==='550px' && obstacle.style.right==='50px' && car.style.right==='50px')|| (obstacle.style.top==='550px' && obstacle.style.left==='50px' && car.style.left==='50px')){
+    obstacle.style.animationName='';
+}
