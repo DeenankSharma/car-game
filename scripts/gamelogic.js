1238e9bfe393087d79ce8b1d8a6544dcc3b23fb8
DeenankSharma <sharmadeenank@gmail.com>
2024-03-01 01:29:10 +0530

    feat : obstacle speed increases automatically

--- scripts/gamelogic.js
+++ scripts/gamelogic.js
@@ -3,28 +3,29 @@ const obstacle=document.querySelector(".obstacle");
 document.addEventListener("keydown",(e)=>{
     if(e.key==="ArrowRight"){
         car.style.right = "50px";
-        console.log(Event)
         car.style.left = '';
     }
     else if(e.key==="ArrowLeft"){
         car.style.left = "50px";
         car.style.right = '';
-        console.log(Event)
     }
 })
 var animationIterations = 0;
 
 obstacle.addEventListener('animationiteration', () => {
     animationIterations++;
-    console.log("Animation iteration: " + animationIterations);
 
     if (animationIterations % 2 === 1) {
-        console.log("Moving obstacle to the right");
         obstacle.style.right = '50px';
         obstacle.style.left = '';
+        
     } else if (animationIterations % 2 === 0) {
-        console.log("Moving obstacle to the left");
         obstacle.style.left = '50px';
         obstacle.style.right = '';
     }
+    let currentStyle = window.getComputedStyle(obstacle);
+    let animationDuration = currentStyle.getPropertyValue('animation-duration');
+    console.log(parseFloat(animationDuration));
+    animationDuration=(parseFloat(animationDuration)/1.05);
+    obstacle.style.setProperty('animation-duration', animationDuration + 's');
 });
