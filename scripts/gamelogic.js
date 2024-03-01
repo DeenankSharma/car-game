6f1581131181a9f903dc4af70493cd12993c92ee
DeenankSharma <sharmadeenank@gmail.com>
2024-03-01 20:25:40 +0530

    feat : adds collision detection

--- scripts/gamelogic.js
+++ scripts/gamelogic.js
@@ -7,7 +7,17 @@ const score=document.querySelector(".score");
 const scoreplus=new Audio('music/sound effects/coin.mp3');
 const clickmusic=new Audio('music/sound effects/click.mp3');
 const btn=document.querySelectorAll("button");
+const division=document.querySelector(".division");
 
+// var obsheight=obstacle.getBoundingClientRect().height;
+// var obswidth=obstacle.getBoundingClientRect().width;
+// var carheight=car.getBoundingClientRect().height;
+// var carwidth=car.getBoundingClientRect().width;
+// var carx=car.getBoundingClientRect().x;
+// var cary=car.getBoundingClientRect().y;
+// var obsx=car.getBoundingClientRect().x;
+// var obsy=car.getBoundingClientRect().y;
+ 
 let scoring=true;
 document.addEventListener("keydown",(e)=>{
     if(e.key==="ArrowRight"){
@@ -24,38 +34,10 @@ btn.forEach((button)=>{
     clickmusic.play();
 })})
 var animationIterations = 0;
-
-obstacle.addEventListener('animationiteration', () => {
-    animationIterations++;
-    score.textContent=("Score :" + animationIterations);
-    if(scoring===true){
-        scoreplus.play();
-    }
-    let x=Math.floor(Math.random()*2);
-    if (x===1) {
-        obstacle.style.right = '50px';
-        obstacle.style.left = '';
-        
-    } else if (x===0) {
-        obstacle.style.left = '50px';
-        obstacle.style.right = '';
-    }
-    // let currentStyle = window.getComputedStyle(obstacle);
-    // let animationDuration = currentStyle.getPropertyValue('animation-duration');
-    // console.log(parseFloat(animationDuration));
-    // animationDuration=(parseFloat(animationDuration)/1.05);
-    // obstacle.style.setProperty('animation-duration', animationDuration + 's');
-});
-
-
-console.log(obstacle.style.top);
-if((obstacle.style.top==='550px' && obstacle.style.right==='50px' && car.style.right==='50px')|| (obstacle.style.top==='550px' && obstacle.style.left==='50px' && car.style.left==='50px')){
-    obstacle.style.animationName='';
-}
 musicbtn.addEventListener("click",()=>{
     if (musicbtnimg.getAttribute("src")==="styles/volume-up.png") {
         musicbtnimg.setAttribute("src","styles/no-sound.png");
-        musicbgfile.setAttribute("src"," ");
+        musicbgfile.setAttribute("src","");
         scoring=false;
     }
     else{
@@ -64,3 +46,74 @@ musicbtn.addEventListener("click",()=>{
         scoring=true;
     }
 })
+obstacle.addEventListener('animationiteration', () => {
+    animationIterations++;
+    score.textContent=("Score :" + animationIterations);
+    if(scoring===true){
+        scoreplus.play();
+    }
+    
+    var x=Math.floor(Math.random()*2);
+    if (x===1) {
+        obstacle.style.right = '50px';
+        obstacle.style.left = '';
+        
+    } else if (x===0) {
+        obstacle.style.left = '50px';
+        obstacle.style.right = '';
+    }
+});
+// function collide(){
+//     const obsheight=obstacle.getBoundingClientRect().height;
+//     const elem=document.elementFromPoint(707,740);
+//     console.log(elem);
+//     if(elem.className==="obstacle"){
+//         obstacle.style.animation='none';
+//         division.style.animation='none';
+//         obstacle.style.transform="translateY(740px)"
+//     }
+// }
+// setInterval(collide,100);
+
+// function getpositions(){
+//     var obsheight=obstacle.getBoundingClientRect().height;
+//     var obswidth=obstacle.getBoundingClientRect().width;
+//     var carheight=car.getBoundingClientRect().height;
+//     var carwidth=car.getBoundingClientRect().width;
+//     var carx=car.getBoundingClientRect().x;
+//     var cary=car.getBoundingClientRect().y;
+//     var obsx=car.getBoundingClientRect().x;
+//     var obsy=car.getBoundingClientRect().y;
+//     if((cary<=(obsy+obsheight)) && carx===obsx){
+//         obstacle.classList.add("paused");
+//         division.classList.add("paused");
+//     }
+// }
+// setInterval(getpositions,500);
+
+function getpositions() {
+    var obsheight = obstacle.getBoundingClientRect().height;
+    var obswidth = obstacle.getBoundingClientRect().width;
+    var carheight = car.getBoundingClientRect().height;
+    var carwidth = car.getBoundingClientRect().width;
+    var carleft = car.getBoundingClientRect().left;
+    var cartop = car.getBoundingClientRect().top;
+    var obsleft = obstacle.getBoundingClientRect().left;
+    var obstop = obstacle.getBoundingClientRect().top;
+    if (
+        cartop<=(obstop+obsheight) &&
+        obstop<=(cartop+carheight) &&
+        carleft<=(obsleft+obswidth) &&
+        obsleft<=(carleft+carwidth)
+    ) {
+        obstacle.classList.add("paused");
+        division.classList.add("paused");
+        
+
+    } else {
+        obstacle.classList.remove("paused");
+        division.classList.remove("paused");
+    }
+}
+
+setInterval(getpositions, 500);
